03e2c4c35e677ed9a07a8627f871faace8266667
Leodan Condori <leodccc7@gmail.com>
2021-03-17 08:54:33 -0500

    hemos modificado nuestro archivo javascript

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log('Por amor al Creador seguiré adelante')
+console.log('Por amor al Creador seguiré adelante');
+console.log('mis pasos en git');
+console.log('no pararé hasta llegar al cielo')
